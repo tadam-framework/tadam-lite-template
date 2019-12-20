25d6a6eb5a5074c6f066a978b696aed35bffaefd
Andros Fenollosa <andros@fenollosa.email>
2019-12-20 19:20:28 +0100

    Update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tadam-lite-template/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tadam-lite-template/lein-template "1.0.0"
+  :description " Minimalist template for fast and light development with Tadam"
+  :url "http://tadam-framework.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
