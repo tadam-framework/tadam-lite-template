c8f2bbdbe71ffdd98884518565023ea1b10e2f67
Andros Fenollosa <andros@fenollosa.email>
2019-12-21 15:39:40 +0100

    Update html

--- src/leiningen/new/tadam_lite.clj
+++ src/leiningen/new/tadam_lite.clj
@@ -13,13 +13,13 @@
     (->files data
              ["README.md" (render "README.md" data)]
              ["project.clj" (render "project.clj" data)]
-             ["lein-env-example" "lein-env-example"]
+             [".lein-env" (render "lein-env-example" data)]
              ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
              ["src/{{sanitized}}/urls.clj" (render "urls.clj" data)]
              ["src/{{sanitized}}/views/public.clj" (render "views/public.clj" data)]
-             ["resources/public/css/main.css" "resources/public/css/main.css"]
-             ["resources/public/js/main.js" "resources/public/js/main.js"]
-             ["resources/public/img/rabbit.svg" "resources/public/img/rabbit.svg"]
-             ["resources/templates/layouts/base.html" "resources/templates/layouts/base.html"]
-             ["resources/templates/public/welcome.html" "resources/templates/public/welcome.html"]
-             ["resources/templates/public/404.html" "resources/templates/public/404.html"])))
+             ["resources/public/css/main.css" (render "resources/public/css/main.css" data)]
+             ["resources/public/js/main.js" (render "resources/public/js/main.js" data)]
+             ["resources/public/img/rabbit.svg" (render "resources/public/img/rabbit.svg" data)]
+             ["resources/templates/layouts/base.html" (render "resources/templates/layouts/base.html" data)]
+             ["resources/templates/public/welcome.html" (render "resources/templates/public/welcome.html" data)]
+             ["resources/templates/public/404.html" (render "resources/templates/public/404.html" data)])))
