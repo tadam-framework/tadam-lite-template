5a55bfa8d9de7769075e028e5e740d422648f7ef
Andros Fenollosa <andros@fenollosa.email>
2020-05-28 00:40:23 +0200

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tadam-lite/lein-template "0.4.3"
+(defproject tadam-lite/lein-template "0.4.4"
   :description "Magic Web framework with Clojure, simple by default"
   :url "http://tadam-framework.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
